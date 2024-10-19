71f8cec4bbb6cd2454636320a34f4541aa9c732b
Kunal Srivastava <kunal@masagroup.com>
2024-10-18 21:47:50 -0700

    frontend searches and hits backend

--- frontend/src/MainPage.js
+++ frontend/src/MainPage.js
@@ -4,59 +4,63 @@ import IconButton from "@mui/material/IconButton/index.js";
 import TextField from "@mui/material/TextField/index.js";
 import Box from "@mui/material/Box/index.js"
 import Button from "@mui/material/Button/index.js";
+import queryEmails from "./api/queryEmails.js";
 
-const SearchBar = ({ setSearchQuery }) => {
-  const handleClick = (event) => {
+const SearchBar = ({ searchResults, setSearchResults }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleClick = async (event) => {
     event.preventDefault();
-    console.log('hello');
+    const result = await queryEmails(searchQuery);
+    const data = result?.metadatas;
+    console.log("received data: ", data);
+    setSearchResults(data);
+  };
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
   };
 
   return (
     <Box sx={{ flexGrow: 1, margin: 2 }}>
-      <form onSubmit={(e) => handleClick(e)}>
-        <TextField
-          id="search-bar"
-          className="text"
-          onInput={(e) => {
-            setSearchQuery(e.target.value);
-          }}
-          label="How can I help you?"
-          variant="outlined"
-          placeholder="Search Mail"
-          sx={{
-            '& .MuiOutlinedInput-root': {
-              borderRadius: '20px',
-              backgroundColor: '#f0f0f0',
-              '& fieldset': {
-                borderColor: 'transparent',
-              },
-              '&:hover fieldset': {
-                borderColor: 'transparent',
-              },
-              '&.Mui-focused fieldset': {
-                borderColor: 'primary.main',
-              },
+      <TextField
+        id="search-bar"
+        className="text"
+        value={searchQuery}
+        onChange={handleChange}
+        label="How can I help you?"
+        variant="outlined"
+        placeholder="Search Mail"
+        sx={{
+          '& .MuiOutlinedInput-root': {
+            borderRadius: '20px',
+            backgroundColor: '#f0f0f0',
+            '& fieldset': {
+              borderColor: 'transparent',
             },
-          }}
-        />
-        <IconButton type="button" aria-label="search" onClick={handleClick}>
-          {/* Add onClick handler here */}
-        </IconButton>
-        <Button variant="contained" color="primary" type="button" onClick={handleClick}>
-          Submit
-        </Button>
-      </form>
+            '&:hover fieldset': {
+              borderColor: 'transparent',
+            },
+            '&.Mui-focused fieldset': {
+              borderColor: 'primary.main',
+            },
+          },
+        }}
+      />
+      <Button variant="contained" color="primary" type="button" onClick={handleClick}>
+        Submit
+      </Button>
     </Box>
   );
 }
 
 function MainPage() {
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
   return (
     <div>
       <h1>Welcome to Your Electron React App</h1>
-      <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <SearchBar searchResults={searchResults} setSearchResults={setSearchResults} />
       {/* Add more components and content here */}
     </div>
   );
