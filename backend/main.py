f08642f0daa7b1021d5ec5a4c904df0fdbdc1b3f
Andrew Shen <andrewshen31@gmail.com>
2024-10-18 19:39:58 -0700

    more updated on the backend

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,45 @@
-from flask import Flask, Request
+from flask import Flask, request
+import chromadb
 import json
 
-# flask --app main --debug run
+# flask --app backend/main --debug run
 
 app = Flask(__name__)
 
-with open("./data/synth_data.json") as fobj:
-    GENERATED_SYNTHETIC_DATA = json.load(fobj)
+client = chromadb.Client()
+collection = client.create_collection(
+    "emails",
+    metadata={"hnsw:space": "cosine"}
+)
+
+
+def synth_initialize():
+    with open("./backend/data/synth_data.json") as fobj:
+        data = json.load(fobj)
+        for id, d in enumerate(data):
+            collection.add(
+                # TODO: merge subject w/ body
+                documents=[d["body"].strip("\n")], # we embed for you, or bring your own
+                metadatas=[{
+                    "from": d["from"],
+                    "to": d["to"],
+                    "subject": d["subject"],
+                    "body": d["body"],
+                }], # filter on arbitrary metadata!
+                ids=[str(id)], # must be unique for each doc
+            )
 
 
 @app.get('/result')
 def return_result():
-    print(GENERATED_SYNTHETIC_DATA)
-    return GENERATED_SYNTHETIC_DATA
+    query = request.args.get("query")
+    print(query)
+    results = collection.query(
+        query_texts=[query],
+        n_results=5,
+    )
+    print(results)
+    return results
 
+
+synth_initialize()
