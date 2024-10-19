70bb46e283873c05b52ad4642bf66a86a74cff80
ayaanrahim <ayaanarahim@gmail.com>
2024-10-18 23:18:24 -0700

    Added email results

--- frontend/src/MainPage.js
+++ frontend/src/MainPage.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { useState } from 'react';
+import IconButton from "@mui/material/IconButton/index.js";
 import TextField from "@mui/material/TextField/index.js";
+import Box from "@mui/material/Box/index.js";
 import Button from "@mui/material/Button/index.js";
 import queryEmails from "./api/queryEmails.js";
+import List from '@mui/material/List/index.js';
+import ListItem from '@mui/material/ListItem/index.js';
+import Divider from '@mui/material/Divider/index.js';
+import ListItemText from '@mui/material/ListItemText/index.js';
+import Typography from '@mui/material/Typography/index.js';
 
 const SearchBar = ({ searchResults, setSearchResults }) => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -21,12 +28,13 @@ const SearchBar = ({ searchResults, setSearchResults }) => {
 
   return (
     <div className="SearchBar">
-      <input
-        type="text"
-        id="search-bar"
-        value={searchQuery}
-        onChange={handleChange}
-        placeholder="How can I help you?"
+      <TextField
+          id="search-bar"
+          value={searchQuery}
+          onChange={handleChange}
+          label="How can I help you?"
+          variant="outlined"
+          placeholder="Search Mail"
       />
       <Button variant="contained" color="primary" onClick={handleClick}>
         Submit
@@ -35,16 +43,45 @@ const SearchBar = ({ searchResults, setSearchResults }) => {
   );
 };
 
+const ResultEmail = ({ searchResult }) => {
+  return (
+      <ListItem className="ListItem">
+          <ListItemText
+              primary={searchResult.subject}
+              secondary={
+                  <React.Fragment>
+                      <Typography
+                          component="span"
+                          variant="body2"
+                          sx={{ color: 'text.primary', display: 'inline' }}
+                          >
+                              {searchResult.to}
+                          </Typography>
+                          {` - ${searchResult.body}`}
+                  </React.Fragment>
+              }
+          />
+      </ListItem>
+  );
+}
+
 function MainPage() {
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState(null);
 
   return (
     <div className="MainPage">
-      <h1 style={{color: 'white', textAlign: 'center'}}>
-        Good Evening, Alex!
-      </h1>
+      <h1>Welcome to Your Electron React App</h1>
       <SearchBar searchResults={searchResults} setSearchResults={setSearchResults} />
-      {/* Add more components and content here */}
+      {searchResults && (
+          <List className="List">
+              {searchResults[0].map((result, index) => (
+                  <React.Fragment key={index}>
+                      <ResultEmail searchResult={result} />
+                      <Divider />
+                  </React.Fragment>
+              ))}
+          </List>
+      )}
     </div>
   );
 }
