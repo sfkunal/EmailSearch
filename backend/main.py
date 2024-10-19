fe1e7a86b5145cbc779fe5dc798dc29049618623
Andrew Shen <andrewshen31@gmail.com>
2024-10-18 21:41:48 -0700

    groq integrated

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from flask import Flask, request
 import chromadb
+from chromadb.utils import embedding_functions
+from groq import Groq
+
 import json
+import re
+import configparser
+
+CONFIG = configparser.ConfigParser()
+CONFIG.read("TOKENS.ini")
+
+GROQ_API_KEY = CONFIG["SECRETS"]["GROQ_API_KEY"]
 
 # flask --app backend/main --debug run
 
@@ -12,14 +22,22 @@ collection = client.create_collection(
     metadata={"hnsw:space": "cosine"}
 )
 
+# embedder = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
+
+client = Groq(
+    api_key=GROQ_API_KEY,
+)
+
 
 def synth_initialize():
     with open("./backend/data/synth_data.json") as fobj:
         data = json.load(fobj)
         for id, d in enumerate(data):
+            merged_data = d["subject"]+" "+re.sub(r"[\n]", " ", d["body"])
+            # embeddings = embedder(merged_data)
+            # print(embeddings)
             collection.add(
-                # TODO: merge subject w/ body
-                documents=[d["body"].strip("\n")], # we embed for you, or bring your own
+                documents=[merged_data], # we embed for you, or bring your own
                 metadatas=[{
                     "from": d["from"],
                     "to": d["to"],
@@ -33,13 +51,47 @@ def synth_initialize():
 @app.get('/result')
 def return_result():
     query = request.args.get("query")
-    print(query)
+    
     results = collection.query(
         query_texts=[query],
         n_results=5,
     )
-    print(results)
+    
     return results
+
+@app.post('/message')
+def ai_message():
+    ids = request.args.get("ids")
+    prompt = request.args.get("prompt")
+
+    results = collection.get(
+        ids=ids.split(","),
+        # where={"style": "style1"}
+    )
+
+    textual_data = ""
+    for ind, metadata in enumerate(results["metadatas"]):
+        textual_data += f"[#{ind}] An email sent from {metadata['from']} to {metadata['to']} with subject line {metadata['subject']} with content of <<<{metadata['body']}>>>\n"
+
+    print(textual_data)
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": f"Helpful assistant that assists users to find their content from emails for the given data, where individual emails are separated by [#]. {textual_data}"
+            },
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="llama3-8b-8192",
+    )
+
+    response = chat_completion.choices[0].message.content
+
+    return response
 
 
 synth_initialize()
